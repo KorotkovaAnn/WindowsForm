cec3e13c76c0df69d1dd8f3b0e1e483f4b652b9b
анечка <145132062+KorotkovaAnn@users.noreply.github.com>
2025-05-04 02:03:33 +0500

    Add files via upload

---
+++ SalesEntryForm/SalesEntryForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsKorotkova.Forms
+{
+    partial class SalesEntryForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboBoxName;
+        private System.Windows.Forms.ComboBox comboBoxBrand;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.ComboBox comboBoxMaterial;
+        private System.Windows.Forms.ComboBox comboBoxEmployee;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.DataGridView dataGridViewSales;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelBrand;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Label labelMaterial;
+        private System.Windows.Forms.Label labelEmployee;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label labelQuantity;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Button buttonShowSales; // Новая кнопка для отображения таблицы
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.comboBoxName = new System.Windows.Forms.ComboBox();
+            this.comboBoxBrand = new System.Windows.Forms.ComboBox();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.comboBoxMaterial = new System.Windows.Forms.ComboBox();
+            this.comboBoxEmployee = new System.Windows.Forms.ComboBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.dataGridViewSales = new System.Windows.Forms.DataGridView();
+            this.buttonShowSales = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelBrand = new System.Windows.Forms.Label();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.labelMaterial = new System.Windows.Forms.Label();
+            this.labelEmployee = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelQuantity = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxName
+            // 
+            this.comboBoxName.Location = new System.Drawing.Point(150, 10);
+            this.comboBoxName.Name = "comboBoxName";
+            this.comboBoxName.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxName.TabIndex = 0;
+            // 
+            // comboBoxBrand
+            // 
+            this.comboBoxBrand.Location = new System.Drawing.Point(150, 50);
+            this.comboBoxBrand.Name = "comboBoxBrand";
+            this.comboBoxBrand.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBrand.TabIndex = 1;
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.Location = new System.Drawing.Point(150, 90);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxColor.TabIndex = 2;
+            // 
+            // comboBoxMaterial
+            // 
+            this.comboBoxMaterial.Location = new System.Drawing.Point(150, 130);
+            this.comboBoxMaterial.Name = "comboBoxMaterial";
+            this.comboBoxMaterial.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMaterial.TabIndex = 3;
+            // 
+            // comboBoxEmployee
+            // 
+            this.comboBoxEmployee.Location = new System.Drawing.Point(150, 170);
+            this.comboBoxEmployee.Name = "comboBoxEmployee";
+            this.comboBoxEmployee.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEmployee.TabIndex = 4;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(150, 210);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 5;
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(150, 250);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(100, 20);
+            this.textBoxQuantity.TabIndex = 6;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "dd.MM.yyyy";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(150, 290);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 7;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(150, 330);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Сохранить продажу";
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // dataGridViewSales
+            // 
+            this.dataGridViewSales.Location = new System.Drawing.Point(20, 380);
+            this.dataGridViewSales.Name = "dataGridViewSales";
+            this.dataGridViewSales.Size = new System.Drawing.Size(600, 200);
+            this.dataGridViewSales.TabIndex = 10;
+            this.dataGridViewSales.Visible = false;
+            // 
+            // buttonShowSales
+            // 
+            this.buttonShowSales.Location = new System.Drawing.Point(320, 330);
+            this.buttonShowSales.Name = "buttonShowSales";
+            this.buttonShowSales.Size = new System.Drawing.Size(172, 23);
+            this.buttonShowSales.TabIndex = 9;
+            this.buttonShowSales.Text = "Вывести таблицу продаж";
+            this.buttonShowSales.Click += new System.EventHandler(this.buttonShowSales_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.Location = new System.Drawing.Point(20, 10);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(100, 23);
+            this.labelName.TabIndex = 11;
+            this.labelName.Text = "Наименование";
+            // 
+            // labelBrand
+            // 
+            this.labelBrand.Location = new System.Drawing.Point(20, 50);
+            this.labelBrand.Name = "labelBrand";
+            this.labelBrand.Size = new System.Drawing.Size(100, 23);
+            this.labelBrand.TabIndex = 12;
+            this.labelBrand.Text = "Бренд";
+            // 
+            // labelColor
+            // 
+            this.labelColor.Location = new System.Drawing.Point(20, 90);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(100, 23);
+            this.labelColor.TabIndex = 13;
+            this.labelColor.Text = "Цвет";
+            // 
+            // labelMaterial
+            // 
+            this.labelMaterial.Location = new System.Drawing.Point(20, 130);
+            this.labelMaterial.Name = "labelMaterial";
+            this.labelMaterial.Size = new System.Drawing.Size(100, 23);
+            this.labelMaterial.TabIndex = 14;
+            this.labelMaterial.Text = "Материал";
+            // 
+            // labelEmployee
+            // 
+            this.labelEmployee.Location = new System.Drawing.Point(20, 170);
+            this.labelEmployee.Name = "labelEmployee";
+            this.labelEmployee.Size = new System.Drawing.Size(100, 23);
+            this.labelEmployee.TabIndex = 15;
+            this.labelEmployee.Text = "Сотрудник";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.Location = new System.Drawing.Point(20, 210);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(100, 23);
+            this.labelPrice.TabIndex = 16;
+            this.labelPrice.Text = "Цена";
+            // 
+            // labelQuantity
+            // 
+            this.labelQuantity.Location = new System.Drawing.Point(20, 250);
+            this.labelQuantity.Name = "labelQuantity";
+            this.labelQuantity.Size = new System.Drawing.Size(100, 23);
+            this.labelQuantity.TabIndex = 17;
+            this.labelQuantity.Text = "Количество";
+            // 
+            // labelDate
+            // 
+            this.labelDate.Location = new System.Drawing.Point(20, 290);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(100, 23);
+            this.labelDate.TabIndex = 18;
+            this.labelDate.Text = "Дата продажи";
+            // 
+            // SalesEntryForm
+            // 
+            this.ClientSize = new System.Drawing.Size(650, 600);
+            this.Controls.Add(this.comboBoxName);
+            this.Controls.Add(this.comboBoxBrand);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.comboBoxMaterial);
+            this.Controls.Add(this.comboBoxEmployee);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonShowSales);
+            this.Controls.Add(this.dataGridViewSales);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelBrand);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.labelMaterial);
+            this.Controls.Add(this.labelEmployee);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelQuantity);
+            this.Controls.Add(this.labelDate);
+            this.Name = "SalesEntryForm";
+            this.Text = "Добавление продажи";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSales)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ SalesEntryForm/SalesEntryForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsKorotkova.Forms
+{
+    public partial class SalesEntryForm : Form
+    {
+        public SalesEntryForm()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(SalesEntryForm_Load);
+        }
+
+        private void SalesEntryForm_Load(object sender, EventArgs e)
+        {
+            LoadComboBoxes();
+            LoadSalesData(); // Initially load sales data
+        }
+
+        private void LoadComboBoxes()
+        {
+            try
+            {
+                using (SqlConnection conn = DatabaseHelper.GetOpenConnection())
+                {
+                    if (conn == null)
+                    {
+                        MessageBox.Show("Не удалось подключиться к базе данных.");
+                        return;
+                    }
+
+                    void Load(string query, ComboBox comboBox, string displayField, string valueField)
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        comboBox.DataSource = dt;
+                        comboBox.DisplayMember = displayField;
+                        comboBox.ValueMember = valueField;
+
+                        if (dt.Rows.Count == 0)
+                            MessageBox.Show($"Пустой источник данных для {comboBox.Name}");
+                    }
+
+                    Load("SELECT код_наименование, наименование FROM наименование", comboBoxName, "наименование", "код_наименование");
+                    Load("SELECT код_цвета, цвет FROM цвет", comboBoxColor, "цвет", "код_цвета");
+                    Load("SELECT код_материала, материал FROM материал", comboBoxMaterial, "материал", "код_материала");
+                    Load("SELECT код_бренд, бренд FROM бренд", comboBoxBrand, "бренд", "код_бренд");
+                    Load("SELECT код_сотрудника, фамилия + ' ' + имя AS ФИО FROM сотрудники", comboBoxEmployee, "ФИО", "код_сотрудника");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке ComboBox: " + ex.Message);
+            }
+        }
+
+        private int GetNextSaleId(SqlConnection conn)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT MAX(CAST(код_продажи AS INT))+1 FROM продажи;", conn);
+            return (int)cmd.ExecuteScalar();
+        }
+
+        private int GetProductId(SqlConnection conn)
+        {
+            SqlCommand cmd = new SqlCommand(@"
+                SELECT TOP 1 код_товара FROM товар 
+                WHERE наименование = @nameId AND цвет = @colorId AND материал_обуви = @materialId AND бренд = @brandId", conn);
+
+            cmd.Parameters.AddWithValue("@nameId", comboBoxName.SelectedValue);
+            cmd.Parameters.AddWithValue("@colorId", comboBoxColor.SelectedValue);
+            cmd.Parameters.AddWithValue("@materialId", comboBoxMaterial.SelectedValue);
+            cmd.Parameters.AddWithValue("@brandId", comboBoxBrand.SelectedValue);
+
+            object result = cmd.ExecuteScalar();
+            if (result == null)
+            {
+                MessageBox.Show("Товар с выбранными параметрами не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return -1;
+            }
+
+            return Convert.ToInt32(result);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (!decimal.TryParse(textBoxPrice.Text, out decimal price) ||
+                !int.TryParse(textBoxQuantity.Text, out int quantity))
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения цены и количества.");
+                return;
+            }
+
+            using (SqlConnection conn = DatabaseHelper.GetOpenConnection())
+            {
+                if (conn == null) return;
+
+                int saleId = GetNextSaleId(conn);
+                int productId = GetProductId(conn);
+
+                if (productId == -1) return;
+
+                SqlCommand cmd = new SqlCommand(@"
+                    INSERT INTO продажи (код_продажи, товар, цена, количество, дата, сотрудник)
+                    VALUES (@saleId, @productId, @price, @quantity, @date, @employeeId)", conn);
+
+                cmd.Parameters.AddWithValue("@saleId", saleId);
+                cmd.Parameters.AddWithValue("@productId", productId);
+                cmd.Parameters.AddWithValue("@price", price);
+                cmd.Parameters.AddWithValue("@quantity", quantity);
+                cmd.Parameters.AddWithValue("@date", dateTimePicker.Value.ToString("dd.MM.yyyy"));
+                cmd.Parameters.AddWithValue("@employeeId", comboBoxEmployee.SelectedValue);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Продажа успешно добавлена.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadSalesData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при добавлении продажи: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadSalesData()
+        {
+            using (SqlConnection conn = DatabaseHelper.GetOpenConnection())
+            {
+                if (conn == null) return;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(@"
+                    SELECT 
+                        s.код_продажи, 
+                        n.наименование AS Наименование, 
+                        cl.цвет AS Цвет, 
+                        m.материал AS Материал, 
+                        b.бренд AS Бренд, 
+                        s.цена, 
+                        s.количество, 
+                        s.дата, 
+                        emp.фамилия + ' ' + emp.имя AS Сотрудник
+                    FROM продажи s
+                    JOIN товар t ON s.товар = t.код_товара
+                    JOIN наименование n ON t.наименование = n.код_наименование
+                    JOIN цвет cl ON t.цвет = cl.код_цвета
+                    JOIN материал m ON t.материал_обуви = m.код_материала
+                    JOIN бренд b ON t.бренд = b.код_бренд
+                    JOIN сотрудники emp ON s.сотрудник = emp.код_сотрудника", conn);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                // Форматируем дату
+                foreach (DataRow row in dt.Rows)
+                {
+                    if (row["дата"] is DateTime dtValue)
+                    {
+                        row["дата"] = dtValue.ToString("dd.MM.yyyy");
+                    }
+                }
+
+                dataGridViewSales.DataSource = dt;
+            }
+        }
+
+        private void buttonShowSales_Click(object sender, EventArgs e)
+        {
+            LoadSalesData();
+            dataGridViewSales.Visible = true; // Показать таблицу при нажатии на кнопку
+        }
+    }
+}
